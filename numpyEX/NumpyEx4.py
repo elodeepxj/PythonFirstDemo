f9d378c87b306603fa713b31c7edcc112a8c2008
pengxiaojie_shanlin <99133950@qq.com>
2017-12-28 15:38:31 +0800

    update numpy

--- numpyEX/NumpyEx4.py
+++ numpyEX/NumpyEx4.py
@@ -12,9 +12,16 @@ c,v = np.loadtxt("f:/kaggle/titanic/train.csv",delimiter=',',usecols=(0,1),unpac
 print c,v
 
 print "计算平均值"
-print np.average(c)#平均值
-print np.average(c,weights=v)# 加权平均值
-print np.mean(c)#平均值
-print np.max(c)#最大值
-print np.min(c)#最小值
-print np.ptp(c)#最大值和最小值的差值
+print "平均值:",np.average(c)#平均值
+print "加权平均值:",np.average(c,weights=v)# 加权平均值
+print "平均值:",np.mean(c)#平均值
+print "最大值:",np.max(c)#最大值
+print "最小值:",np.min(c)#最小值
+print "最大值和最小值的差值:",np.ptp(c)#最大值和最小值的差值
+
+print "中位数:",np.median(c)#中位数
+print "方差：",np.var(c)#方差
+
+print np.mean((c-np.mean(c))**2)#方差公式
+print np.diff(c)#相邻元素差值
+print np.where(v>0)#条件搜索，返回索引值
